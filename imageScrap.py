a743a9d2f25ad2bd61ce473086cfc8fcef1c09dc
Huzbi-crypto <huzbinaru@gmail.com>
2023-10-24 22:42:58 +0500

    Changes to be committed: 	modified:   imageScrap.py

--- imageScrap.py
+++ imageScrap.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://www.buondua.com"  # Replace with the website's URL
+URL = "https://www.wallpaperflare.com"  # Replace with the website's URL
 # getURL = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
 
 # corsApiHost = "https://cors-anywhere.herokuapp.com/"
-# URL = f"{corsApiHost}buondua.com/"
+# URL = f"{corsApiHost}wallpaperflare.com/"
 getURL = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
 
 soup = BeautifulSoup(getURL.text, 'html.parser')
