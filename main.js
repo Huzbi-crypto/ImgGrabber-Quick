7ca0c1b9abd4ad1257971369dd39d1f8797153e3
Huzbi <76440095+Huzbi-crypto@users.noreply.github.com>
2023-11-04 12:59:01 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 // LOGIC: Download all images from a webpage
 async function downloadAllImages() {
-    const images = document.getElementsByTagName('img');
-    for (i = 0; i < images.length; i++) {
-        const img = images[i];
-        const url = img.src;
-        const response = await fetch(url);
-        const blob = await response.blob();
-        const urlObject = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = urlObject;
-        link.download = `image-${i}.jpg`;
-        link.click();
-        URL.revokeObjectURL(urlObject);
-    }
+   const divs = document.getElementsByClassName('album-photo my-2');
+   let i = 0;
+   for (let div of divs) {
+       const images = div.getElementsByTagName('img');
+       for (let img of images) {
+           const url = img.src;
+           const response = await fetch(url);
+           const blob = await response.blob();
+           const urlObject = URL.createObjectURL(blob);
+           const link = document.createElement('a');
+           link.href = urlObject;
+           link.download = `image-${i}.jpg`;
+           link.click();
+           URL.revokeObjectURL(urlObject);
+           i++;
+       }
+   }
 }
 downloadAllImages();
